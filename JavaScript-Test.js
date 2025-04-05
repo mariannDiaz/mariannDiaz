9b5b1b85d16fe69ec091bb22e2ce8aa9632b28ac
Mariann Diaz Piñero <marianndiazpinero@gmail.com>
2025-04-04 20:54:34 -0700

    Add files via upload

---
+++ JavaScript-Test.js
@@ -0,0 +1,50 @@
+/**
+ * Multi-line
+ * Comment test
+ *  */
+
+var number = 5; // in-line comment
+
+number = 9;
+
+/* Data types:
+
+   undefined, null, boolean, string, 
+   symbol, number, and object
+   
+*/
+
+// There are 'three' ways to declare variables in JavaScript
+
+// 1st. Can be used through the 'whole' program
+var myName = "Mariann"
+
+myName = 8
+
+// 2nd. Will only be used within the scope of where you declared that
+let ourName = "freeCodeCamp"
+
+// 3rd. Is a variable that can never change its stored value
+const pi = 3.1415
+
+var a; // Declare
+
+a = 7; // Assign
+
+var b = 3; // Declare and Assign 
+
+function sum(a,b)
+{
+    return a + b;
+}
+
+console.log(a)    // Allows you to see things in the console
+
+
+
+
+
+
+
+
+
